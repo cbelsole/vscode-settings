b6c74d62c05fb9e058069823e88d73a1c2d98a32
Chris Belsole <cbelsole@paperlesspost.com>
2016-07-27 14:06:20 -0400

    added setting for indent guides and more extentions

--- setup.rb
+++ setup.rb
@@ -10,10 +10,14 @@ user_settings = JSON.parse(File.read(user_settings_path))
 repo_settings = JSON.parse(File.read(settings_path))
 File.open(user_settings_path, 'w') { |file| file.write(JSON.pretty_generate(repo_settings.merge!(user_settings))) }
 
+system 'code --install-extension CraigMaslowski.erb'
+system 'code --install-extension PeterJausovec.vscode-docker'
+system 'code --install-extension fabianlauer.vs-code-xml-format'
+system 'code --install-extension groksrc.haml'
 system 'code --install-extension lukehoban.Go'
-system 'code --install-extension xabikos.JavaScriptSnippets'
+system 'code --install-extension mohsen1.prettify-json'
 system 'code --install-extension rebornix.Ruby'
 system 'code --install-extension samverschueren.final-newline'
-system 'code --install-extension groksrc.haml'
-system 'code --install-extension PeterJausovec.vscode-docker'
 system 'code --install-extension seanmcbreen.Spell'
+system 'code --install-extension xabikos.JavaScriptSnippets'
+system 'code --install-extension ziyasal.vscode-open-in-github'
